39049e1527877958c065400926dedf36b38ca114
JaimeGrimaldo <DankerGamer-@hotmail.com>
2023-10-03 00:46:45 -0600

    Preparando home

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Paginas/login/login.component';
 import { RouterModule } from '@angular/router';
 import { RegistroComponent } from './Paginas/registro/registro.component';
+import { HomeComponent } from './Paginas/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegistroComponent],
+  declarations: [AppComponent, LoginComponent, RegistroComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'registro', component: RegistroComponent },
+      { path: 'home', component: HomeComponent },
       { path: '', redirectTo: '/login', pathMatch: 'full' },
     ]),
   ],
