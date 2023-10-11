f4a7e514ebc4d53a4a6801c5ab80273abf8612cb
JaimeGrimaldo <DankerGamer-@hotmail.com>
2023-10-11 08:28:39 -0600

    vista configuracion de cuenta

---
+++ FrontEnd/src/app/Paginas/configuracion-cuenta/configuracion-cuenta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-cuenta',
+  templateUrl: './configuracion-cuenta.component.html',
+  styleUrls: ['./configuracion-cuenta.component.css']
+})
+export class ConfiguracionCuentaComponent {
+
+}
